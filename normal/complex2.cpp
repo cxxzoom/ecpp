0ecb420253c089da8a0b59338b7a9c72177baf37
ucdo <phpsarc@gmail.com>
2023-07-20 08:45:53 +0800

    Update complex2.cpp

--- normal/complex2.cpp
+++ normal/complex2.cpp
@@ -3,56 +3,61 @@
 //
 
 #include <iostream>
+
 #define CALL_WITH (a, b ,f(a > b ? a : b));
 
 using namespace std;
-void f(int num){
+
+void f(int num) {
     cout << endl << "f() : " << num << endl;
 }
 
 
-class Foo{
+class Foo {
 public:
-    Foo(){
+    Foo() {
 
     };
     const int a = 1;
     const int b = 2;
-    ~Foo(){ callWithMax(this->a,this->b);};
+
+    ~Foo() { callWithMax(this->a, this->b); };
     // enum hack
-    enum{A = 5};
+    enum {
+        A = 5
+    };
     char arr[A];
 
 private:
-
     template<typename T>
-    inline void  callWithMax(const T& a,const T& b){
+    inline void callWithMax(const T &a, const T &b) {
         f(a > b ? a : b);
     }
 };
 
-void T(const Foo *f){};
+void T(const Foo *f) {};
 
-int main()
-{
+int main() {
     char arr[] = "David";
-    for (const  auto &n:arr) {
+    for (const auto &n: arr) {
         cout << n << endl;
     }
-    const char* const C = "123";
+    const char *const C = "123";
     const std::string Name("David");
     // err C = "999";
     cout << C << endl;
     cout << Name << endl;
     cout << size(arr);
-    int a = 1,b=0;
-    CALL_WITH(++a,b);
-    CALL_WITH(++a,b);
+    int a = 1, b = 0;
+    CALL_WITH
+    (++a, b);
+    CALL_WITH
+    (++a, b);
 
     cout << "-==========================-" << endl;
     Foo foo;
 
-    std::string const  name = "1";
+    std::string const name = "1";
 //    name = 2;
 
 }
